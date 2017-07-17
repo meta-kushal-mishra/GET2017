9b4d0a0ec6a20adc5dcded774c72a2f874f53c44
meta-kushal-mishra <kushal.mishra@metacube.com>
2017-07-17 14:32:42 +0530

    Update OctalConversion.java

--- PF/PF-Assignment1/OctalConversion.java
+++ PF/PF-Assignment1/OctalConversion.java
@@ -1,16 +1,15 @@
-
-
 import java.lang.*;
-public class OctalConversion {
+public class OctalConversion{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
 		int result = OctalConversion.convertBinaryToOctal(111101);
-		//System.out.print(result);
-
 	}
+	
+	// method to convert binary to octal giving binary value as argument
 	public static int convertBinaryToOctal(int binaryValue){
-		int remainder=0,octalValue=0,j=1,decimalValue=0;
+		int remainder=0,octalValue=0,j=1,decimalValue=0,i=0;
+		
+		// converts binary to decimal
 		while(binaryValue!=0){
 			if(remainder!=0 && remainder!=1){
 				return -1;
@@ -20,9 +19,9 @@ public class OctalConversion {
 			j = j*2;
 			binaryValue = binaryValue/10;
 		}
-		//System.out.println(decimalValue);
 		remainder=0;
-		int i=0;
+		
+		// converts decimal to octal
 		while(decimalValue!=0){
 			remainder = decimalValue % 8;
 			octalValue = (int) (octalValue + remainder*Math.pow(10, i));
