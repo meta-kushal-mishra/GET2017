57ab39e406f4edfce22b3b52f16c6f4fbc1a7f33
meta-kushal-mishra <kushal.mishra@metacube.com>
2017-07-17 15:54:44 +0530

    Update RemoveDuplicates.java

--- PF/PF-Assignment1/RemoveDuplicates.java
+++ PF/PF-Assignment1/RemoveDuplicates.java
@@ -8,8 +8,8 @@ public class RemoveDuplicates {
 		}
 	}
 	
-	public static void merge(int arr[], int l, int m, int r)
-	{
+	public static void merge(int arr[], int l, int m, int r){
+		
 	    // Find sizes of two subarrays to be merged
 	    int n1 = m - l + 1;
 	    int n2 = r - m;
@@ -32,15 +32,12 @@ public class RemoveDuplicates {
 	
 	    // Initial index of merged subarry array
 	    int k = l;
-	    while (i < n1 && j < n2)
-	    {
-	        if (L[i] <= R[j])
-	        {
+	    while (i < n1 && j < n2){
+	        if (L[i] <= R[j]){
 	            arr[k] = L[i];
 	            i++;
 	        }
-	        else
-	        {
+	        else{
 	            arr[k] = R[j];
 	            j++;
 	        }
@@ -48,16 +45,14 @@ public class RemoveDuplicates {
 	    }
 	
 	    /* Copy remaining elements of L[] if any */
-	    while (i < n1)
-	    {
+	    while (i < n1){
 	        arr[k] = L[i];
 	        i++;
 	        k++;
 	    }
 	
 	    /* Copy remaining elements of R[] if any */
-	    while (j < n2)
-	    {
+	    while (j < n2){
 	        arr[k] = R[j];
 	        j++;
 	        k++;
@@ -66,10 +61,8 @@ public class RemoveDuplicates {
 	
 	// Main function that sorts arr[l..r] using
 	// merge()
-	public static void sort(int arr[], int l, int r)
-	{
-	    if (l < r)
-	    {
+	public static void sort(int arr[], int l, int r){
+	    if (l < r){
 	        // Find the middle point
 	        int m = (l+r)/2;
 	
@@ -81,13 +74,12 @@ public class RemoveDuplicates {
 	        merge(arr, l, m, r);
 	    }
 	}
-	public static int[] removeDuplicates(int arr[] )
-    {	
+	public static int[] removeDuplicates(int arr[] ){	
 		int n = arr.length;
 		sort(arr,0,n-1);
         // Return, if array is empty
         // or contains a single element
-        if (n==0 || n==1)
+       	 if (n==0 || n==1)
             return arr;
       
         int[] temp = new int[n];
