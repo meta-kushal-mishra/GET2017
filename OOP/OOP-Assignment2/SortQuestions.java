8627411ced89e0e5085509fe13c0b59b488d3b46
meta-kushal-mishra <kushal.mishra@metacube.com>
2017-07-21 19:26:12 +0530

    OOP Assignment 2 solutions

---
+++ OOP/OOP-Assignment2/SortQuestions.java
@@ -0,0 +1,27 @@
+package oop2;
+
+import java.util.Arrays;
+
+public class SortQuestions{
+
+	private Question[] ques;
+	private ReadQuestions read = new ReadQuestions("C:/Users/Kushal/workspace/PF1/src/oop2/questions.txt");
+	
+	
+	// Method to sort the questions based on the question text
+	public void sortQuestions() {
+		String[] lines = read.readLines();	
+		ques = new SurveyApplication().separateLines(lines);		//creating the question objects
+		
+		Arrays.sort(ques);
+		//printing the sorted array
+		for(int i=0;i<ques.length;i++) {
+			System.out.println(ques[i].getQuestion());
+		}
+	}
+	
+	public static void main(String[] args) {
+		SortQuestions sortQues = new SortQuestions();
+		sortQues.sortQuestions();
+	}
+}
