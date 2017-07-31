3b1973c02ed80d1b3b8aac048a952f65cada7732
meta-kushal-mishra <kushal.mishra@metacube.com>
2017-07-31 14:29:06 +0530

    OOP Assignment 5 solution

---
+++ OOP/OOP-Assignment5/ActionMenu.java
@@ -0,0 +1,9 @@
+package oop4;
+
+import java.util.Scanner;
+
+public interface ActionMenu {
+
+	
+	public void actionTaken(Scanner scan);
+}
---
+++ OOP/OOP-Assignment5/Menu.java
@@ -0,0 +1,11 @@
+package oop4;
+
+
+public abstract class Menu implements ActionMenu{
+
+	public abstract void setName(String name);
+	public abstract String getName();
+
+	
+	
+}
