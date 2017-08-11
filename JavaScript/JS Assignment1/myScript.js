7dee8208fb5e3df15cd3ceefe8266f6e455a3e55
meta-kushal-mishra <kushal.mishra@metacube.com>
2017-08-11 13:07:18 +0530

    css added

--- JavaScript/JS Assignment1/myScript.js
+++ JavaScript/JS Assignment1/myScript.js
@@ -1,18 +1,16 @@
-function redirect() 
-{
-    window.location="page.html";
+function redirect() {
+  window.location="page.html";
 }
-function redirectToContact()
-{
-	window.location="Contacts.html";
+
+function redirectToContact(){
+  window.location="Contacts.html";
 }
-function validateForm()
-{
+
+function validateForm(){
  	var x=document.getElementById("name");
  	var namepattern = /^([a-zA-Z]|\s)*$/;
 
- 	if (!namepattern.test(x.value) || x.value=="")
- 	{
+ 	if (!namepattern.test(x.value) || x.value==""){
     if(x.value=="")
       alert("Name is required. Please enter your name");
     else
@@ -23,8 +21,7 @@ function validateForm()
 
  	var emailpattern = /\S+@\S+\.\S+/ ;
 	x=document.getElementById("email");
- 	if(!emailpattern.test(x.value) || x.value=="")
-  {
+ 	if(!emailpattern.test(x.value) || x.value==""){
     if(x.value=="")
       alert("Email is required. Please enter your email");
     else
@@ -36,53 +33,49 @@ function validateForm()
  if(!validateSelect())
     return false;
 
- 	var phoneno = /^\d{10}$/;
- 	x=document.getElementById("contact");
- 	if(!phoneno.test(x.value) || x.value=="")
-  {
-    if(x.value=="")
-      alert("Phone No. is required. Please enter your phone number");
-    else
- 		alert("Invalid no .");
-  		x.focus();
-      return false;
- 	}
-
- 	var orgReg = /[a-zA-Z]/;
- 	x=document.getElementById("orgName");
- 	if(!orgReg.test(x.value) || x.value=="")
- 	{
+  var orgReg = /[a-zA-Z]/;
+  x=document.getElementById("orgName");
+  if(!orgReg.test(x.value) || x.value==""){
     if(x.value==""){
       alert("Enter Org name");
     }
     else
- 		   alert("Invalid orgName ");
-  		x.focus();
+       alert("Invalid orgName ");
+      x.focus();
       return false;
+  }
+
+ 	var phoneno = /^\d{10}$/;
+ 	x=document.getElementById("contact");
+ 	if(!phoneno.test(x.value) || x.value==""){
+    if(x.value=="")
+      alert("Phone No. is required. Please enter your phone number");
+    else
+ 		   alert("Invalid no .");
+  	x.focus();
+    return false;
  	}
+
+ 
  	var message = document.getElementById("Message");
- 	if(message.length > 250)
- 	{
+ 	if(message.value.length > 250){
  		alert("Invalid message");
   		x.focus();
       return false;
  	}
  }
- function validateSelect()
- {
+ function validateSelect(){
  	var x =document.getElementById("select");
- 	if (x.value=="Choose here") 
- 	{
+ 	if (x.value=="Choose here") {
     	alert("Please make a selection");
         x.focus();
         return false;
     }
-    else
-    {
+    else{
     	var y = document.getElementById("textField");
     	y.defaultValue = x.value;
     	document.getElementById('textField').style.visibility='visible';
-		document.getElementById('City').style.visibility='visible';
+		  document.getElementById('City').style.visibility='visible';
     return true;
     }
 
