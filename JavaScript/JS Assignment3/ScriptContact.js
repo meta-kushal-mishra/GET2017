4e54367a528720687b284ed2c7e0ba6b56181950
meta-kushal-mishra <kushal.mishra@metacube.com>
2017-08-17 10:52:16 +0530

    improved indentation

--- JavaScript/JS Assignment3/ScriptContact.js
+++ JavaScript/JS Assignment3/ScriptContact.js
@@ -12,8 +12,7 @@ function removeLoc(){
 
 
 function validateForm(){
-	if(validateName() && validateEmail() && validateCity() && validateOrganization() && validateNumber() && validateMessage())
-	{
+	if(validateName() && validateEmail() && validateCity() && validateOrganization() && validateNumber() && validateMessage()){
 		document.form.name.value="";
 		document.form.email.value="";
 		document.form.organization.value="";
@@ -26,8 +25,7 @@ function validateForm(){
 	return false;
 }
 
-function validateName()
-{
+function validateName(){
 	var name = document.form.name;
 	var len = name.value.length;
 	var letters1 = /^[A-Za-z]+ +[A-Za-z]+$/;
@@ -38,8 +36,7 @@ function validateName()
 		localStorage.setItem("key1",name.value);
 		return true;
 	}	
-	else
-	{
+	else{
 		name.value="";
 		name.style.outlineColor="red";
 		alert("Name should have 1 to 20 charachters");
@@ -48,18 +45,21 @@ function validateName()
 	}
 }
 
-function validateEmail()
-{
+function validateEmail(){
 	var email= document.form.email;
 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
 	
-	if(email.value.match(mailformat) )
-	{
+	if(email.value.match(mailformat)){
 		localStorage.setItem("key2",email.value);
 		return true;
 	}
-	else
-	{
+	else if(email.value.length==0){
+		email.style.outlineColor="red";
+		alert("Email address is mandatory");
+		email.focus();
+		return false;
+	}
+	else{
 		email.value="";
 		email.style.outlineColor="red";
 		alert("Enter a valid email address");
@@ -68,13 +68,19 @@ function validateEmail()
 	}
 }
 
-function validateOrganization()
-{
+function validateOrganization(){
 	var organization = document.form.organization;
 	var letters = /^[A-Za-z]+$/;
 	if(organization.value.match(letters)){
 		localStorage.setItem("key4",organization.value);
 		return true;
+	}
+	else if(organization.value.length==0){
+		organization.value="";
+		organization.style.outlineColor="red";
+		alert("Organization name is mandatory");
+		organization.focus();
+		return false;
 	}	
 	else{
 		organization.value="";
@@ -85,19 +91,23 @@ function validateOrganization()
 	}
 }
 
-function validateNumber()
-{
+function validateNumber(){
 	var num = document.form.num;
 	var numbers = /^[0-9]+$/;  
 	var len = num.value.length;
 
-	if(num.value.match(numbers) && len==10 )
-	{
+	if(num.value.match(numbers) && len==10 ){
 		localStorage.setItem("key5",num.value);
 		return true;
 	}
-	else
-	{
+	else if(len ==0){
+		num.value="";
+		num.style.outlineColor="red";
+		alert("Phone num is mandatory");
+		num.focus();
+		return false;
+	}
+	else{
 		num.value="";
 		num.style.outlineColor="red";
 		alert("Enter a valid number of 10 digits for contact numbers");
@@ -106,12 +116,10 @@ function validateNumber()
 	}
 }
 
-function validateMessage()
-{
+function validateMessage(){
 	var msg = document.form.msg;
 
-	if(msg.value.length > 250 || msg.value.length == 0)
-	{
+	if(msg.value.length > 250 || msg.value.length == 0){
 		msg.value="";
 		msg.style.outlineColor="red";
 		alert("Enter 1 to 250 charachters for message");
@@ -122,12 +130,10 @@ function validateMessage()
 	return true;
 }
 
-function validateCity()
-{
+function validateCity(){
 	var city = document.form.city;
 	localStorage.setItem("key3",city.value);
-	if(city.value ==  "")
-	{
+	if(city.value ==  ""){
 		alert("Select a city from drop down list");
 		return false;
 	}
@@ -135,8 +141,7 @@ function validateCity()
 	return true;
 }
 
-function fillField(city)
-{
+function fillField(city){
 	var name = city.value;
 	document.form.cityname.hidden = false;   
 	document.form.cityname.value = "Selected city is :" + name;
